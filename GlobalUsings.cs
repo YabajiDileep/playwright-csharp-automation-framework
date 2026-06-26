a5810b32e01f8669133aaf53ac7d9c0b8d89d42d
DileepYabaji <38105178+YabajiDileep@users.noreply.github.com>
2026-06-26 08:24:27 +0530

    Delete GlobalUsings.cs

--- GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-global using Microsoft.Playwright;
-global using NUnit.Framework;
-global using System.Text.Json;
-global using AventStack.ExtentReports;
-global using AventStack.ExtentReports.Reporter;
